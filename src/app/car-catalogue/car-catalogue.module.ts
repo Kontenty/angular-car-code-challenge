6b37a8abdbf382ce653065564a2af4f0a44465e7
Konrad Gorski <konradgrsk@gmail.com>
2023-10-23 22:12:10 +0200

    feat: connect car-form to formControl, clear subtype if type changed

--- src/app/car-catalogue/car-catalogue.module.ts
+++ src/app/car-catalogue/car-catalogue.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+
 import { CarCatalogueComponent } from './car-catalogue/car-catalogue.component';
 import { CarFormComponent } from './car-form/car-form.component';
 import { CarDisplayComponent } from './car-display/car-display.component';
 
 @NgModule({
   declarations: [CarCatalogueComponent, CarFormComponent, CarDisplayComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   exports: [CarCatalogueComponent],
 })
 export class CarCatalogueModule {}
--- src/app/car-catalogue/car-form/car-form.component.ts
+++ src/app/car-catalogue/car-form/car-form.component.ts
@@ -1,15 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-car-form',
   templateUrl: './car-form.component.html',
-  styleUrls: ['./car-form.component.css']
+  styleUrls: ['./car-form.component.css'],
 })
-export class CarFormComponent implements OnInit {
+export class CarFormComponent {
+  carForm = this.formBuilder.group({
+    type: '',
+    subType: '',
+    licensePlate: '',
+  });
 
-  constructor() { }
+  carOptions = [
+    { id: 'hatchback', name: 'Hatchback' },
+    { id: 'sedan', name: 'Sedan' },
+    { id: 'station', name: 'Station' },
+    { id: 'cabriolet', name: 'Cabriolet' },
+    { id: 'coupe', name: 'Coupé' },
+    { id: 'mvp', name: 'Multi Purpose Vehicle (MVP)' },
+    { id: 'terreinauto', name: 'Terreinauto' },
+  ];
 
-  ngOnInit(): void {
+  motorOptions = [
+    { id: 'allroad', name: 'All-road' },
+    { id: 'naked', name: 'Naked' },
+    { id: 'enduro', name: 'Enduro' },
+    { id: 'race', name: 'Race' },
+    { id: 'toermotor', name: 'Toermotor' },
+    { id: 'chopper', name: 'Chopper' },
+    { id: 'zijspan', name: 'Zijspan' },
+  ];
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  getSubTypeOptions() {
+    const subTypeOptions: Record<string, { id: string; name: string }[]> = {
+      auto: this.carOptions,
+      motor: this.motorOptions,
+      scooter: [],
+    };
+    return this.carForm.value?.type
+      ? subTypeOptions[this.carForm.value.type]
+      : [];
   }
 
+  handleVehicleChange() {
+    this.carForm.patchValue({ subType: '' });
+  }
 }
